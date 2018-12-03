39964aff70813f2bc0cfc67d0c29bd68400ea0a5
André P. Bertoletti <apbertoletti@gmail.com>
2018-12-02 23:37:52 -0200

    Add project files.

---
+++ DevPiraFestival/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevPiraFestival
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
